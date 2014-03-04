23a9379308dbdf6e578ec66bd6c1ae2101f1296d
samohtj <thomas.jonathan.c@gmail.com>
2014-03-03 21:06:41 -0500

    Header Files
    Added new method of finding the distance to the next basket, and changed
    some c source files to header files.

---
+++ Robot Code/Autonomous/autonomous-includes/multiplexer.h
@@ -0,0 +1,64 @@
+/*
+*	Include some necessary files
+*/
+#include "hitechnic-sensormux.h"
+#include "lego-light.h"
+#include "hitechnic-irseeker-v2.h"
+#include "hitechnic-touchmux.h"
+#include "hitechnic-gyro.h"
+
+/*
+*	Declare some sendor constants
+*/
+const tMUXSensor irLeft = msensor_S2_1;
+const tMUXSensor irRight = msensor_S2_2;
+const tMUXSensor lightSenseLeft 	= msensor_S2_3;
+const tMUXSensor lightSenseRight 	= msensor_S2_4;
+
+/*
+*	Variables to store the sensor values
+*/
+int rawLightLeft;
+int rawLightRight;
+int irStrengthLeft;
+int irStrengthRight;
+//int gyroVel;
+
+bool touchInput1;
+bool touchInput2;
+bool touchInput3;
+
+int dummy;
+
+/*
+*	Constantly update the variables with the sensor values
+*/
+task getSmux()
+{
+	LSsetActive(lightSenseLeft);											// Turn the light sensors on to show that we're working
+	LSsetActive(lightSenseRight);
+	writeDebugStreamLine("Multiplexer setup ready");	// Print some info
+
+	while (true){
+		rawLightLeft = LSvalRaw(lightSenseLeft);				// Set all the variables to the sensor readings
+		rawLightRight = LSvalRaw(lightSenseRight);
+
+		if(!HTIRS2readEnhanced(irLeft, dummy, irStrengthLeft)){
+			writeDebugStreamLine("Something's wrong");
+		}
+		if(!HTIRS2readEnhanced(irRight, dummy, irStrengthRight)){
+			writeDebugStreamLine("Something's wrong");
+		}
+
+		//gyroVel = HTGYROreadRot(msensor_S2_1);
+
+		//writeDebugStreamLine("IR Left:%d IR Right: %d", irStrengthLeft, irStrengthRight);
+
+		//touchInput1 = HTTMUXisActive(TMUX, 1);
+		//touchInput2 = HTTMUXisActive(TMUX, 2);
+		//touchInput3 = HTTMUXisActive(TMUX, 3);
+
+	}
+
+
+}
